2eb5649df6ac74cdfd6cf7259aa463aa15c1c33b
Matijas Levang <micolevang@gmail.com>
2025-09-15 21:07:00 +0200

    Initial commit

---
+++ pki-backend/src/main/java/com/example/pkibackend/PkiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pkibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkiBackendApplication.class, args);
+	}
+
+}
