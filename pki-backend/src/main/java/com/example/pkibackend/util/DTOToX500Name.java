9ac26f370421b2306c6e01e0a25fa7f94a9fc789
Matijas Levang <micolevang@gmail.com>
2025-09-20 17:02:50 +0200

    [ADD] Util class for converting Issuer and Subject DTOs into their respective X500Names

---
+++ pki-backend/src/main/java/com/example/pkibackend/util/DTOToX500Name.java
@@ -0,0 +1,35 @@
+package com.example.pkibackend.util;
+
+import com.example.pkibackend.certificates.dtos.IssuerDTO;
+import com.example.pkibackend.certificates.dtos.SubjectDTO;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.X500NameBuilder;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+
+public class DTOToX500Name {
+    public static X500Name IssuerDTOToX500Name(IssuerDTO issuerDTO) {
+        X500NameBuilder builder = new X500NameBuilder();
+        builder.addRDN(BCStyle.CN, issuerDTO.getGivenName() + " " + issuerDTO.getSurname());
+        builder.addRDN(BCStyle.SURNAME, issuerDTO.getSurname());
+        builder.addRDN(BCStyle.GIVENNAME, issuerDTO.getGivenName());
+        builder.addRDN(BCStyle.O, issuerDTO.getOrganization());
+        builder.addRDN(BCStyle.OU, issuerDTO.getDepartment());
+        builder.addRDN(BCStyle.C, issuerDTO.getCountry());
+        builder.addRDN(BCStyle.E, issuerDTO.getEmail());
+        builder.addRDN(BCStyle.UID, issuerDTO.getUuid());
+        return builder.build();
+    }
+
+    public static X500Name SubjectDTOToX500Name(SubjectDTO subjectDTO) {
+        X500NameBuilder builder = new X500NameBuilder();
+        builder.addRDN(BCStyle.CN, subjectDTO.getGivenName() + " " + subjectDTO.getSurname());
+        builder.addRDN(BCStyle.SURNAME, subjectDTO.getSurname());
+        builder.addRDN(BCStyle.GIVENNAME, subjectDTO.getGivenName());
+        builder.addRDN(BCStyle.O, subjectDTO.getOrganization());
+        builder.addRDN(BCStyle.OU, subjectDTO.getDepartment());
+        builder.addRDN(BCStyle.C, subjectDTO.getCountry());
+        builder.addRDN(BCStyle.E, subjectDTO.getEmail());
+        //builder.addRDN(BCStyle.UID, subjectDTO.getUuid());
+        return builder.build();
+    }
+}
