57d498bde4fe6b3d489faf2b17da487a6bffdb0d
Milica Stanojlovic <mstanojlovic222@gmail.com>
2025-09-22 19:08:54 +0200

    delete comments

--- pki-frontend/src/app/auth/auth.service.ts
+++ pki-frontend/src/app/auth/auth.service.ts
@@ -22,18 +22,3 @@ export class AuthService {
   getProfile() { return this.kc.loadUserProfile(); }
 }
 
-// import { Injectable } from '@angular/core';
-// import { KeycloakService } from 'keycloak-angular';
-
-// @Injectable({ providedIn: 'root' })
-// export class AuthService {
-//   constructor(private kc: KeycloakService) {}
-
-//   login() { return this.kc.login({ redirectUri: window.location.origin + '/' }); }
-//   logout() { return this.kc.logout(window.location.origin + '/'); }
-//   register() { return this.kc.register({ redirectUri: window.location.origin + '/' }); }
-
-//   isLoggedIn() { return this.kc.isLoggedIn(); }
-//   async token() { return await this.kc.getToken(); }
-//   async profile() { return await this.kc.loadUserProfile(); }
-// }
